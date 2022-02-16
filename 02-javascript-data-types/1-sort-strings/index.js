37bed02e22030f35402868784e7ef1c7e1a69e3a
“zascia” <zascia@ukr.net>
2022-02-16 15:04:17 +0200

    test tasks 1,2,3 all done

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,20 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+function wayOfCompare(a,b) {
+  // !!! this one setting had been taken from the teacher's repository after multiple tryings to solve it by myself
+  //  ['ru', 'en'], {caseFirst: 'upper'}
+  return a.localeCompare(b, ['ru', 'en'], {caseFirst: 'upper'});
+}
 
+export function sortStrings(arr, param = 'asc') {
+  let clonnedArray = arr.slice();
+  let sortedArray = clonnedArray.sort(wayOfCompare);
+
+
+  if (param === 'asc') {
+    return sortedArray;
+  }
+
+  return sortedArray.reverse()
 }
